22a12c574b6afd5b889d4f584f5bc5a37e0188ae
caishu <caishu@polaristech.io>
2018-11-27 23:55:07 +0800

    add dummy eureka server

---
+++ src/main/java/io/polaristech/eurekaDemoServer/EurekaDemoServerApplication.java
@@ -0,0 +1,14 @@
+package io.polaristech.eurekaDemoServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaDemoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDemoServerApplication.class, args);
+	}
+}
